b451cd74cd46487aac046d4f32ec9bbb7d3ecce6
maxgta <maxgta@163.com>
2017-05-05 18:46:00 +0800

    people can't see edit button unless he is group owner

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,9 +18,10 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    @group.user = current_user
 
     if @group.save
-      redirect_to group_path
+      redirect_to groups_path
     else
       render :new
     end
